b09e8e863bb6938b0e5e9c71172269fdb5eb65ea
Mazogu <michael_doom@hotmail.com>
2018-01-12 00:26:17 -0500

    Can add people to a list of people

--- app/src/main/java/com/example/micha/camerapersoninterest/PersonActivity.java
+++ app/src/main/java/com/example/micha/camerapersoninterest/PersonActivity.java
@@ -1,13 +1,50 @@
 package com.example.micha.camerapersoninterest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.micha.camerapersoninterest.model.Person;
+
+import java.util.ArrayList;
 
 public class PersonActivity extends AppCompatActivity {
+
+    private EditText job;
+    private EditText hobby;
+    private EditText name;
+    private EditText age;
+    private EditText gender;
+    private ArrayList<Person> people;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+
+        name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
+        gender = findViewById(R.id.gender);
+        hobby = findViewById(R.id.hobby);
+        job = findViewById(R.id.dream);
+        people = new ArrayList<Person>();
+    }
+
+    public void addPerson(View view) {
+        String pName = name.getText().toString();
+        String pAge = age.getText().toString();
+        String pGender = gender.getText().toString();
+        String pHobby = hobby.getText().toString();
+        String pJob = job.getText().toString();
+        Person person = new Person(pName,pAge,pGender,pHobby,pJob);
+        people.add(person);
+        Toast.makeText(this, "Person: "+person.toString()+" added.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void viewPeople(View view) {
+        Intent intent = new Intent(getApplicationContext(),PersonListActivity.class);
     }
 }
---
+++ app/src/main/java/com/example/micha/camerapersoninterest/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.example.micha.camerapersoninterest.utils;
+
+/**
+ * Created by micha on 1/11/2018.
+ */
+
+public class Constants {
+    public static class VALUES{
+        public static final int MONTHS = 12;
+        public static final int NONZERO = 1;
+    }
+
+    public static class KEYS{
+
+    }
+}
